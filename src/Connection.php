072328891fad7796c1c36a5e0975681b2065186f
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2020-06-15 14:57:23 -0300

    SQLite COUNT updated

--- src/Connection.php
+++ src/Connection.php
@@ -24,7 +24,7 @@
 		abstract protected function buildQuery(string $type) : string;
 		# query functions
 		abstract public function select(string $from, string $what = '*', string $where = null) : object;
-		abstract public function count(string $from, string $what = '*', string $where = null) : object;
+		# abstract public function count(string $from, string $what = '*', string $where = null) : object;
 		abstract public function insert(string $table, string $what, string $values) : void;
 		abstract public function create(string $table, string $columns) : void;
 		abstract public function drop(string $table) : void;
--- src/databases/SQLite.php
+++ src/databases/SQLite.php
@@ -21,45 +21,43 @@
 		protected function buildQuery(string $type) : string {
 			switch ($type) {
 				case queryTypes::SELECT:
-					if (\func_num_args() == 3 || \func_num_args() == 4) {
+					if (\func_num_args() == 2 || \func_num_args() == 3 || \func_num_args() == 4) {
 						$from = \func_get_arg(1);
-						$what = \func_get_arg(2);
-						# if the 4th param exist $where will receive its values, else it'll receive null
+						# if the 3rd  param exist $what will receive its value, else it'll receive '*'
+						$what = (\func_num_args() >= 3) ? \func_get_arg(2) : '*';
+						# if the 4th param exist $where will receive its value, else it'll receive null
 						$where = (\func_num_args() == 4) ? \func_get_arg(3) : null;
 						
 						return ($where == null) ? "SELECT $what FROM `$from`;" : "SELECT $what FROM `$from` WHERE $where;";
 					}
 					else {
-						throw new \Exception('The function expects 3 or 4 params but it receives ' . \func_num_args());
+						throw new \Exception('The function expects 2 to 4 params but it receives ' . \func_num_args());
 					}
 
 					break;
 				case queryTypes::COUNT:
-					if (\func_num_args() == 3 || \func_num_args() == 4) {
+					if (\func_num_args() == 2 || \func_num_args() == 3) {
 						$from = \func_get_arg(1);
-						$what = \func_get_arg(2);
-						# if the 3rd parameter exist $where will receive its values, else it'll receive null
-						$where = (\func_num_args() == 4) ? \func_get_arg(3) : null;
+						# if the 3rd parameter exist $where will receive its value, else it'll receive null
+						$where = (\func_num_args() == 3) ? \func_get_arg(2) : null;
 						
-						return ($where != null) ? "SELECT COUNT($what) FROM `$from` WHERE $where;" : "SELECT COUNT($what) FROM `$from`;";
+						return ($where != null) ? "SELECT COUNT(*) FROM `$from` WHERE $where;" : "SELECT COUNT(*) FROM `$from`;";
 					}
 					else {
-						throw new \Exception('The function expects 3 or 4 params but it receives ' . \func_num_args());
+						throw new \Exception('The function expects 2 or 3 params but it receives ' . \func_num_args());
 					}
 
 					break;
 				case queryTypes::INSERT:
-					if (\func_num_args() == 3 || \func_num_args() == 4) {
+					if (\func_num_args() == 4) {
 						$table = \func_get_arg(1);
-						# if the function receive 4 params $what will receive its values, else it'll receive null
-						$what = (\func_num_args() == 4) ? \func_get_arg(2) : null;
-						# if the 3rd param exist $values will receive its value, else it'll receive the 2nd param's value
-						$values = (\func_num_args() == 3) ? \func_get_arg(2) : \func_get_arg(3);
+						$what = \func_get_arg(2);
+						$values = \func_get_arg(3);
 						
-						return ($what != null) ? "INSERT INTO `$table` ($what) VALUES ($values);" : "INSERT INTO `$table` VALUES ($values);";
+						return "INSERT INTO `$table` ($what) VALUES ($values);";
 					}
 					else {
-						throw new \Exception('The function expects 3 or 4 params but it receives ' . \func_num_args());
+						throw new \Exception('The function expects 4 params but it receives ' . \func_num_args());
 					}
 
 					break;
@@ -116,8 +114,8 @@
 			return $this->connection->query($this->buildQuery(queryTypes::SELECT, "$from", "$what", ($where == null) ? null : "$where"));
 		}
 	
-		public function count(string $from, string $what = '*', string $where = null) : object {
-			return $this->connection->query($this->buildQuery(queryTypes::COUNT, "$from", "$what", ($where == null) ? null : "$where"));
+		public function count(string $from, string $where = null) : object {
+			return $this->connection->query($this->buildQuery(queryTypes::COUNT, "$from", ($where == null) ? null : "$where"));
 		}
 	
 		public function insert(string $table, string $what, string $values) : void {
@@ -136,25 +134,25 @@
 			$this->connection->exec($this->buildQuery(queryTypes::TRUNCATE, "$table"));
 		}
 
-		public function delete(string $table, $where = null) : void {
-			$this->connection->exec($this->buildQuery(queryTypes::DELETE, "$table", ($where == null) ? null : "$where"));
+		public function delete(string $table, string $where) : void {
+			$this->connection->exec($this->buildQuery(queryTypes::DELETE, "$table", "$where"));
 		}
 	
 		# SQL functions
-		function nextResult(\SQLite3Result $result) {
+		public function nextResult(\SQLite3Result $result) {
 			return (gettype($result) != 'boolean') ? $result->fetchArray() : false;
 		}
 	
-		function numRows(\SQLite3Result $result) : int {
+		public function numRows(\SQLite3Result $result) : int {
 			return (gettype($result) != 'boolean') ? \sqlite_num_rows($result) : false;
 		}
 	
-		function affectedRows() : int {
+		public function affectedRows() : int {
 			return $this->connection->changes();
 		}
 
 		# SQLite functions
-		function prepare(string $type) : \SQLite3Stmt {
+		public function prepare(string $type) : \SQLite3Stmt {
 			if (\func_num_args() == 2)
 				return $this->connection->prepare($this->buildQuery($type, \func_get_arg(1)));
 			elseif (\func_num_args() == 3)
