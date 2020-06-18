fd330d61a31ce60c01b1971cac0a8cc2cfcf6fb3
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2020-06-18 13:44:36 -0300

    add tests and the method update

--- src/Connection.php
+++ src/Connection.php
@@ -6,6 +6,7 @@
 		const SELECT = 'select';
 		const COUNT = 'count';
 		const INSERT = 'insert';
+		const UPDATE = 'update';
 		const CREATE = 'create';
 		const DROP = 'drop';
 		const TRUNCATE = 'truncate';
@@ -26,6 +27,7 @@
 		abstract public function select(string $from, string $what = '*', string $where = null) : object;
 		# abstract public function count(string $from, string $what = '*', string $where = null) : object;
 		abstract public function insert(string $table, string $what, string $values) : void;
+		abstract public function update(string $from, string $what, string $where = null) : void;
 		abstract public function create(string $table, string $columns) : void;
 		abstract public function drop(string $table) : void;
 		abstract public function truncate(string $table) : void;
