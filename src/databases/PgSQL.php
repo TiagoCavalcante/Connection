34e645b4e212b3268943598fbf94f0ae33f8a873
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2020-06-26 10:38:58 -0300

    improve sintax

--- src/databases/PgSQL.php
+++ src/databases/PgSQL.php
@@ -23,43 +23,43 @@
 	
 		# functions
 		# generate function (to generate the SQL code)
-		protected function buildQuery(string $type) : string {
-			switch ($type) {
+		protected function buildQuery(array $args) : string {
+			switch ($args[0]) {
 				case queryTypes::SELECT:
-					$from = \func_get_arg(1);
-					$what = \func_get_args()[2] ?? '*';
-					$where = \func_get_args()[3] ?? null;
+					$from = $args[1];
+					$what = $args[2] ?? '*';
+					$where = $args[3] ?? null;
 					
 					return ($where == null) ? "SELECT $what FROM $from;" : "SELECT $what FROM $from WHERE $where;";
 
 					break;
 				case queryTypes::COUNT:
-					$from = \func_get_arg(1);
-					$what = \func_get_arg(2);
-					$where = \func_get_arg(3) ?? null;
+					$from = $args[1];
+					$what = $args[2];
+					$where = $args[3] ?? null;
 
 					return ($where != null) ? "SELECT COUNT(*) FROM $from WHERE $where;" : "SELECT COUNT(*) FROM $from;";
 
 					break;
 				case queryTypes::INSERT:
-					$table = \func_get_arg(1);
-					$what = \func_get_arg(2);
-					$values = \func_get_arg(3);
+					$table = $args[1];
+					$what = $args[2];
+					$values = $args[3];
 					
 					return "INSERT INTO $table ($what) VALUES ($values);";
 
 					break;
 				case queryTypes::UPDATE:
-					$from = \func_get_arg(1);
-					$what = \func_get_arg(2);
-					$where = \func_get_args()[3] ?? null;
+					$from = $args[1];
+					$what = $args[2];
+					$where = $args[3] ?? null;
 					
 					return ($where != null) ? "UPDATE $from SET $what WHERE $where;" : "UPDATE $from SET $what;";
 
 					break;
 				case queryTypes::CREATE:
-					$table = \func_get_arg(1);
-					$columns = \func_get_arg(2);
+					$table = $args[1];
+					$columns = $args[2];
 					
 					$query = "CREATE TABLE IF NOT EXISTS $table (";
 					
@@ -83,20 +83,20 @@
 					
 					break;
 				case queryTypes::DROP:
-					$table = \func_get_arg(1);
+					$table = $args[1];
 					
 					return "DROP TABLE IF EXISTS $table;";
 
 					break;
 				case queryTypes::TRUNCATE:
-					$table = \func_get_arg(1);
+					$table = $args[1];
 					
 					return "TRUNCATE $table;";
 
 					break;
 				case queryTypes::DELETE:
-					$from = \func_get_arg(1);
-					$where = \func_get_arg(2);
+					$from = $args[1];
+					$where = $args[2];
 
 					return "DELETE FROM $from WHERE $where;";
 
@@ -105,35 +105,35 @@
 		}
 		# query functions
 		public function select(string $from, string $what = '*', string $where = null) : object {
-			return $this->connection->query($this->buildQuery(queryTypes::SELECT, $from, $what, $where));
+			return $this->connection->query($this->buildQuery([queryTypes::SELECT, $from, $what, $where]));
 		}
 	
 		public function count(string $from, string $what = '*', string $where = null) : object {
-			return $this->connection->query($this->buildQuery(queryTypes::COUNT, $from, $what, $where));
+			return $this->connection->query($this->buildQuery([queryTypes::COUNT, $from, $what, $where]));
 		}
 	
 		public function insert(string $table, string $what, string $values) : void {
-			$this->connection->exec($this->buildQuery(queryTypes::INSERT, $table, $what, $values));
+			$this->connection->exec($this->buildQuery([queryTypes::INSERT, $table, $what, $values]));
 		}
 
 		public function update(string $from, string $what, string $where = null) : void {
-			$this->connection->exec($this->buildQuery(queryTypes::UPDATE, $from, $what, $where));
+			$this->connection->exec($this->buildQuery([queryTypes::UPDATE, $from, $what, $where]));
 		}
 
 		public function create(string $table, array $columns) : void {
-			$this->connection->exec($this->buildQuery(queryTypes::CREATE, $table, $columns));
+			$this->connection->exec($this->buildQuery([queryTypes::CREATE, $table, $columns]));
 		}
 
 		public function drop(string $table) : void {
-			$this->connection->exec($this->buildQuery(queryTypes::DROP, $table));
+			$this->connection->exec($this->buildQuery([queryTypes::DROP, $table]));
 		}
 
 		public function truncate(string $table) : void {
-			$this->connection->exec($this->buildQuery(queryTypes::TRUNCATE, $table));
+			$this->connection->exec($this->buildQuery([queryTypes::TRUNCATE, $table]));
 		}
 
 		public function delete(string $table, string $where) : void {
-			$this->connection->exec($this->buildQuery(queryTypes::DELETE, $table, $where));
+			$this->connection->exec($this->buildQuery([queryTypes::DELETE, $table, $where]));
 		}
 
 		# SQL functions
@@ -143,7 +143,7 @@
 
 		# PostgreSQL functions
 		public function prepare(string $type) : \PDOStatement {
-			return $this->connection->prepare($this->buildQuery($type, \func_get_arg(1), \func_get_args()[2] ?? null, \func_get_args()[3] ?? null));
+			return $this->connection->prepare($this->buildQuery(\func_get_args()));
 		}
 	}
 ?>
