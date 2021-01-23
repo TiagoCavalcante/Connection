1535439f58e8f92701c7c46220a9de94da2e6e37
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-01-23 16:48:59 -0300

    changed >> to >

--- tests.sh
+++ tests.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
+# grant permission to execute
+chmod +x vendor/bin/phpunit
+
 # autoload test
 ./vendor/bin/phpunit tests/AutoloadFileTest.php
 
 # create env file
 touch env.php
-# grant permission to execute
-chmod 777 vendor/bin/phpunit
+
 # MySQL test
-cat MySQL.env.php >> env.php
+cat MySQL.env.php > env.php
 ./vendor/bin/phpunit tests/ConnectionClassTest.php
-# PgSQL
-cat PgSQL.env.php >> env.php
+# MySQL test
+cat PgSQL.env.php > env.php
 ./vendor/bin/phpunit tests/ConnectionClassTest.php
 # SQLite test
-cat SQLite.env.php >> env.php
+cat SQLite.env.php > env.php
 ./vendor/bin/phpunit tests/ConnectionClassTest.php
+
 # delete env file
 rm env.php
