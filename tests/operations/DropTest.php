9cf7245ccdd72b187a81b5066e0cca234c4da277
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-01-25 18:28:10 -0300

    add Drop class tests

---
+++ tests/operations/DropTest.php
@@ -0,0 +1,16 @@
+<?php
+	final class DropTest extends Operation {
+		public function testCanDropATable() : void {
+			$this->connection->table('test')
+				->drop()
+				->run();
+
+			$this->expectException(Exception::class);
+
+			$this->connection->table('test')
+				->select()
+				->what(['*'])
+				->run();
+		}
+	}
+?>
