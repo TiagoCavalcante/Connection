4772cc823b6e34460f39fc6e511c417b65dd1f07
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-01-25 17:28:13 -0300

    add --stop-on-failure to phpunit call and move run database tests to Bash function

--- tests.sh
+++ tests.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 
+tests () {
+	./vendor/bin/phpunit --stop-on-failure tests/operation/Count.php
+	./vendor/bin/phpunit --stop-on-failure tests/operation/Create.php
+	./vendor/bin/phpunit --stop-on-failure tests/operation/Delete.php
+	./vendor/bin/phpunit --stop-on-failure tests/operation/Drop.php
+	./vendor/bin/phpunit --stop-on-failure tests/operation/Insert.php
+	./vendor/bin/phpunit --stop-on-failure tests/operation/Select.php
+	./vendor/bin/phpunit --stop-on-failure tests/operation/Truncate.php
+	./vendor/bin/phpunit --stop-on-failure tests/operation/Update.php
+}
+
 # grant permission to execute
 chmod +x vendor/bin/phpunit
 
 # autoload test
-./vendor/bin/phpunit tests/AutoloadFileTest.php
+./vendor/bin/phpunit --stop-on-failure tests/Autoload.php
 
 # create env file
 touch env.php
 
 # MySQL test
 cat MySQL.env.php > env.php
-./vendor/bin/phpunit tests/ConnectionClassTest.php
+tests
 # PgSQL test
 cat PgSQL.env.php > env.php
-./vendor/bin/phpunit tests/ConnectionClassTest.php
+tests
 # SQLite test
 cat SQLite.env.php > env.php
-./vendor/bin/phpunit tests/ConnectionClassTest.php
+tests
 
 # delete env file
 rm env.php
