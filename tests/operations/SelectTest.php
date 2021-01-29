beb2c7e7455174e8c3020a433a172b2d90bf702a
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-01-29 14:10:39 -0300

    add orderBy tests

--- tests/operations/SelectTest.php
+++ tests/operations/SelectTest.php
@@ -50,5 +50,101 @@
 
 			$this->truncate();
 		}
+
+		public function testCanDoASelectOrderBy() : void {
+			$this->connection->table('test')
+				->insert()
+				->what('text')
+				->values('C')
+				->run();
+			$this->connection->table('test')
+				->insert()
+				->what('text')
+				->values('A')
+				->run();
+			$this->connection->table('test')
+				->insert()
+				->what('text')
+				->values('A')
+				->run();
+			$this->connection->table('test')
+				->insert()
+				->what('text')
+				->values('B')
+				->run();
+
+			$rows = $this->connection->table('test')
+				->select()
+				->what('id', 'text')
+				->orderBy(['text'], ['id', 'DESC'])
+				->run();
+
+			$this->assertEquals([
+				'id' => 3,
+				0 => 3,
+				'text' => 'A',
+				1 => 'A'
+			], $rows[0]);
+
+			$this->assertEquals([
+				'id' => 2,
+				0 => 2,
+				'text' => 'A',
+				1 => 'A'
+			], $rows[1]);
+
+			$this->assertEquals([
+				'id' => 4,
+				0 => 4,
+				'text' => 'B',
+				1 => 'B'
+			], $rows[2]);
+
+			$this->assertEquals([
+				'id' => 1,
+				0 => 1,
+				'text' => 'C',
+				1 => 'C'
+			], $rows[3]);
+
+			$this->truncate();
+		}
+
+		public function testCanDoASelectWhereOrderBy() : void {
+			$this->connection->table('test')
+				->insert()
+				->what('text')
+				->values('B')
+				->run();
+			$this->connection->table('test')
+				->insert()
+				->what('text')
+				->values('A')
+				->run();
+			$this->connection->table('test')
+				->insert()
+				->what('text')
+				->values('A')
+				->run();
+
+			$rows = $this->connection->table('test')
+				->select()
+				->what('id')
+				->where(['=', 'text', 'A'])
+				->orderBy(['id', 'DESC'])
+				->run();
+
+			$this->assertEquals([
+				'id' => 3,
+				0 => 3,
+			], $rows[0]);
+
+			$this->assertEquals([
+				'id' => 2,
+				0 => 2,
+			], $rows[1]);
+
+			$this->truncate();
+		}
 	}
 ?>
