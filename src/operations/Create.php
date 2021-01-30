150af32c2595b8f79dfdd510a531c708167c6437
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-01-30 14:39:47 -0300

    add default character set to MySQL and MariaDB

--- src/operations/Create.php
+++ src/operations/Create.php
@@ -49,7 +49,15 @@
 				$i++;
 			}
 
-			$query .= ');';
+			if ($this->name === 'MySQL') {
+				$query .= ') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;';
+			}
+			else if ($this->name === 'MariaDB') {
+				$query .= ') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;';
+			}
+			else {
+				$query .= ');';
+			}
 
 			$this->connection->exec($query);
 		}
